cf2d359730628971403f591be235f6205997566f
Edward Silva <edwsilvaoliveira@gmail.com>
2023-11-28 15:59:36 -0300

    Fix : LinkCard Recipe

--- src/Components/Recipes.tsx
+++ src/Components/Recipes.tsx
@@ -28,7 +28,7 @@ export default function Recipes({ title, recipes, categories }: RecipesType) {
       </button>
       {recipes.slice(0, 12).map((recipe, index) => (
         <div key={ recipe[`id${title}`] } data-testid={ `${index}-recipe-card` }>
-          <Link to={ `/${titlePath}/:${recipe[`id${title}`]}` }>
+          <Link to={ `/${titlePath}s/${recipe[`id${title}`]}` }>
             <img
               src={ recipe[`str${title}Thumb`] }
               alt={ recipe[`str${title}`] }
