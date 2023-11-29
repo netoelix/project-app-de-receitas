c9e25941d0f07edf70381063c9418877e4adcd04
Elix Neto <neto.elix@outlook.com>
2023-11-29 17:18:34 -0300

    Feat: estilizando pagina de Login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form';
 import validator from 'validator';
 import { useNavigate } from 'react-router-dom';
 import { requestApi } from '../Utils/ApiRequest';
+import { ContainerDiv, LoginContainer } from '../styles/StyledLogin';
 
 function Login() {
   const { register, getValues, handleSubmit, formState: { isValid } } = useForm();
@@ -19,37 +20,40 @@ function Login() {
   teste();
 
   return (
-    <div>
+    <>
+      <ContainerDiv>
+        <img src="src/images/logo-recipes-app.svg" alt="" />
+        {/* <ImgContainer>
+          <img src="src/images/tomate_sem_fundo.png" alt="" />
+        </ImgContainer> */}
+      </ContainerDiv>
+      <LoginContainer>
+        <h1>LOGIN</h1>
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Email"
+          { ...register(
+            'email',
+            { required: true, validate: (value) => validator.isEmail(value) },
+          ) }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Password"
+          { ...register('password', { minLength: 7, required: true }) }
+        />
+        <button
+          data-testid="login-submit-btn"
+          onClick={ () => handleSubmit(handleButtonEnter)() }
+          disabled={ !isValid }
+        >
+          Enter
+        </button>
+      </LoginContainer>
 
-      <h1>LOGIN</h1>
-
-      <input
-        type="email"
-        data-testid="email-input"
-        placeholder="Email"
-        { ...register(
-          'email',
-          { required: true, validate: (value) => validator.isEmail(value) },
-        ) }
-      />
-
-      <input
-        type="password"
-        data-testid="password-input"
-        placeholder="Password"
-        { ...register('password', { minLength: 7, required: true }) }
-      />
-
-      <button
-        data-testid="login-submit-btn"
-        onClick={ () => handleSubmit(handleButtonEnter)() }
-        disabled={ !isValid }
-
-      >
-        Enter
-      </button>
-
-    </div>
+    </>
 
   );
 }
---
+++ src/styles/StyledLogin.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+  h1 {
+      margin-bottom: 2rem;
+      font-family: 'Roboto', sans-serif;
+    }
+    input {
+        padding: 1rem;
+        margin-bottom: 1rem;
+        width: 300px;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+    }
+    
+    button {
+        padding: 1rem;
+        width: 200px;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+        background-color: #ccc;
+        font-size: 1.5rem;
+        font-weight: 600;
+        cursor: pointer;
+    }
+    `;
+export const ContainerDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #41197F;
+    background: linear-gradient(to bottom, #41197F 75.5%, white 34.5%);
+    padding-top: 2rem;
+`;
+
+// export const ImgContainer = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     img {
+//         width: 47vh;
+//         flex-shrink: 0;
+//     }
+// `;
