e138e98e6fb432f73b9e08eab3fe47d64a3fda78
Edward Silva <edwsilvaoliveira@gmail.com>
2023-11-29 14:51:17 -0300

    Feat: Testes SearchBar

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -1,5 +1,6 @@
-import { getByText, screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 import App from '../App';
 import { renderWithRouterAndProvider } from '../Utils/renderWithRouterAndProvider';
 
@@ -11,8 +12,6 @@ describe('Testes do componente Search Bar', () => {
 
     const execSearchBtn = screen.getByTestId('exec-search-btn');
     const input = screen.getByTestId('search-input');
-    // const ingredientRadioBtn = screen.getByTestId('ingredient-search-radio');
-    // const nameRadioBtn = screen.getByTestId('name-search-radio');
     const firstLetterRadioBtn = screen.getByTestId('first-letter-search-radio');
 
     await user.type(input, 'abc');
@@ -22,5 +21,29 @@ describe('Testes do componente Search Bar', () => {
     const alertText = screen.findByText('Your search must have only 1 (one) character');
 
     expect(alertText).toBeInTheDocument();
+  });
+  test('É renderizado um alert ao tentar procurar um pela primeira letra com 2 digitos ou mais no input', async () => {
+    vi.spyOn(global, 'fetch').mockResolvedValueOnce({
+      json: async () => ({ meals: null, drinks: null }),
+      ok: true,
+    } as Response);
+    vi.spyOn(global, 'alert');
+
+    renderWithRouterAndProvider(<App />, { route: '/meals' });
+    const searchBtn = screen.getByTestId('search-top-btn');
+    await userEvent.click(searchBtn);
+    const input = screen.getByTestId('search-input');
+    const checkbox = screen.getByTestId('first-letter-search-radio');
+    const submit = screen.getByTestId('exec-search-btn');
+
+    await userEvent.type(input, 'a');
+    await userEvent.click(checkbox);
+    await userEvent.click(submit);
+
+    await userEvent.type(input, 'ab');
+    await userEvent.click(submit);
+
+    expect(global.alert).toHaveBeenCalled();
+    expect(global.alert).toHaveBeenCalledWith('Your search must have only 1 (one) character');
   });
 });
