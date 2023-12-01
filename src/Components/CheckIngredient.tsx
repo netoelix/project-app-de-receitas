79baec5abe9fb3d885d7723230cf1f391a82d4c5
Edward Silva <107967519+Edward-Guard@users.noreply.github.com>
2023-11-30 22:39:36 -0300

    Faltando coverage R-38

--- src/Components/CheckIngredient.tsx
+++ src/Components/CheckIngredient.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './CheckIngredient.css';
+
 type CheckboxProps = {
   ingredient: string;
   index:number
 };
 
 function CheckIngredient({ ingredient, index } : CheckboxProps) {
+  const { register, getValues } = useForm();
+  const [checked, setChecked] = useState(false);
+
   return (
-    <label data-testid={ `${index}-ingredient-step` } htmlFor={ ingredient }>
-      <input type="checkbox" id={ ingredient } />
+    <label
+      className={ (checked ? 'marked' : '') }
+      data-testid={ `${index}-ingredient-step` }
+      htmlFor={ ingredient }
+    >
+      <input
+        type="checkbox"
+        id={ ingredient }
+        { ...register('checkbox', { onChange: () => setChecked(getValues('checkbox')),
+        }) }
+      />
       {ingredient}
     </label>
   );
