446d18108c4c48b98c40a983a6dd1fdb50442034
Edward Silva <107967519+Edward-Guard@users.noreply.github.com>
2023-12-05 22:26:27 -0300

    Removendo consoles

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { StyledFooter } from '../styles/StyledFooter';
 import { drinkIcon, mealIcon } from '../Utils/exportIcons';
