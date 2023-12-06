af106b5c278405223d9de6ef96461f0ad66a3aef
Elix Neto <neto.elix@outlook.com>
2023-12-06 13:56:05 -0300

    Fix: lint

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ProfileContainer } from '../../styles/StyledProfile';
-import { doneIcon, exitIcon, favoriteIcon, profileYellowIcon } from '../../Utils/exportIcons';
+import { doneIcon, exitIcon,
+  favoriteIcon, profileYellowIcon } from '../../Utils/exportIcons';
 
 function Profile() {
   const [email, setEmail] = useState('');
